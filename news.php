d9145c5c121c2db1435314e4f2ca2b7deee36616
Lyubava <moyfeerverkk@gmail.com>
2021-03-18 20:45:18 +0200

    [WIP]

---
+++ news.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Template Name: News
+ */
+wp_enqueue_style( 'fullpage', get_template_directory_uri() . '/assets/scss/news.scss' );
+
+get_header();
+?>
+
+  s
+
+<?php
+get_footer();
