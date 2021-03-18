c3f4066943b3dd3a857019821808e7ec634e171c
Vadim Prodan <vp.edge@gmail.com>
2021-03-18 18:03:42 +0200

    [WIP]

--- assets/js/fullpage-init.js
+++ assets/js/fullpage-init.js
@@ -1,22 +1,22 @@
 (function ($) {
-    $(document).ready(function() {
-        $('#fullpage').fullpage({
-            css3: true,
-            navigation: true,
-            navigationPosition: 'left',
-            afterRender: function() {
-                $('#fp-nav')
-                    .prepend('<span class="prev"></span>')
-                    .append('<span class="next"></span>');
+  $(document).ready(function () {
+    $('#fullpage').fullpage({
+      css3: true,
+      navigation: true,
+      navigationPosition: 'left',
+      afterRender: function () {
+        $('#fp-nav')
+            .prepend('<span class="prev ti ti-arrow-right ti-rotate-270"></span>')
+            .append('<span class="next ti ti-arrow-right ti-rotate-90"></span>');
 
-                $('#fp-nav').find('span.prev, span.next').on('click', function () {
-                    if ($(this).hasClass('prev')) {
-                        $.fn.fullpage.moveSectionUp();
-                    } else {
-                        $.fn.fullpage.moveSectionDown();
-                    }
-                });
-            }
+        $('#fp-nav').find('span.prev').on('click', function () {
+          $.fn.fullpage.moveSectionUp();
         });
+
+        $('#fp-nav').find('span.next').on('click', function () {
+          $.fn.fullpage.moveSectionDown();
+        });
+      }
     });
+  });
 })(jQuery);
--- index.php
+++ index.php
@@ -5,42 +5,43 @@
  * @package Techno
  */
 
-wp_enqueue_style('fullpage', get_template_directory_uri() . '/assets/css/fullpage.min.css', [], '1.0.0');
+wp_enqueue_style( 'style', get_template_directory_uri() . '/assets/css/default.min.css', [], wp_get_theme()->get( 'Version' ) );
+wp_enqueue_style( 'fullpage', get_template_directory_uri() . '/assets/css/fullpage.min.css', [], wp_get_theme()->get( 'Version' ) );
 
-wp_enqueue_script('fullpage', 'https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.7/jquery.fullpage.min.js', ['jquery'], '2.9.7');
-wp_enqueue_script('fullpage-init', get_template_directory_uri() . '/assets/js/fullpage-init.js', ['jquery', 'fullpage'], '1.0.0');
+wp_enqueue_script( 'fullpage', 'https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.7/jquery.fullpage.min.js', [ 'jquery' ], '2.9.7' );
+wp_enqueue_script( 'fullpage-init', get_template_directory_uri() . '/assets/js/fullpage-init.js', [ 'jquery', 'fullpage' ], wp_get_theme()->get( 'Version' ) );
 
 get_header();
 ?>
 
-<div id="fullpage">
+  <div id="fullpage">
     <div class="section">Some section</div>
     <div class="section">Some section</div>
     <div id="news-and-promotions" class="section">
-        <div class="container d-flex flex-column">
-            <h1 class="fw-bold">Новости и акции</h1>
-            <ul class="d-flex">
-                <li class="item">
-                    <img src="" alt="">
-                    <span>19.09.2020</span>
-                    <span>Сдача ЖК "Европейский квартал" в г. Черновцы</span>
-                </li>
-                <li class="item">
-                    <img src="" alt="">
-                    <span>19.09.2020</span>
-                    <span>Сдача ЖК "Европейский квартал" в г. Черновцы</span>
-                </li>
-                <li class="item">
-                    <img src="" alt="">
-                    <span>19.09.2020</span>
-                    <span>Сдача ЖК "Европейский квартал" в г. Черновцы</span>
-                </li>
-            </ul>
-            <button class="btn btn-danger mx-auto">Все новости</button>
-        </div>
+      <div class="container d-flex flex-column">
+        <h1 class="fw-bold text-primary">Новости и акции</h1>
+        <ul class="d-flex">
+          <li class="item">
+            <img src="" alt="">
+            <span>19.09.2020</span>
+            <span>Сдача ЖК "Европейский квартал" в г. Черновцы</span>
+          </li>
+          <li class="item">
+            <img src="" alt="">
+            <span>19.09.2020</span>
+            <span>Сдача ЖК "Европейский квартал" в г. Черновцы</span>
+          </li>
+          <li class="item">
+            <img src="" alt="">
+            <span>19.09.2020</span>
+            <span>Сдача ЖК "Европейский квартал" в г. Черновцы</span>
+          </li>
+        </ul>
+        <button class="btn btn-danger mx-auto">Все новости</button>
+      </div>
     </div>
     <div class="section">Some section</div>
-</div>
+  </div>
 
 <?php
 get_footer();
