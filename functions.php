99aaa363497dbdf8b0e95f3824f5c87064d63b1c
Vadim Prodan <vp.edge@gmail.com>
2021-03-22 05:49:07 +0200

    [WIP]

--- functions.php
+++ functions.php
@@ -19,6 +19,6 @@ function theme_enqueue_scripts() {
     wp_enqueue_style( 'montserrat-font', 'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap', [], wp_get_theme()->get( 'Version' ) );
     wp_enqueue_style( 'main', get_template_directory_uri() . '/assets/css/main.min.css', [], wp_get_theme()->get( 'Version' ) );
 
-    wp_enqueue_script( 'main', get_template_directory_uri() . '/node_modules/bootstrap/js/dist/modal.js', [], wp_get_theme()->get( 'Version' ) );
+    wp_enqueue_script( 'main', get_template_directory_uri() . '/assets/js/main.js', [ 'jquery' ], wp_get_theme()->get( 'Version' ) );
 }
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_scripts' );
